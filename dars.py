e1c87177fb58533d50f771a7993cf82c634c30bb
Suxrob Sadullayev <saddulayevsuxrob@gmail.com>
2026-06-20 17:01:52 +0500

    darslik

---
+++ dars.py
@@ -0,0 +1,122 @@
+# print("hello world")
+# print("Assalomu alaykum")
+# print("Hayrli tong")
+# print("ahvollar qanday")
+
+# print(99+1)
+# print(100-1)
+# print(10*10)
+# print(100/10)
+# print(10**2)
+# print(10%3)
+# print(10//3)        
+# print("10 ning kvadrati", 10**2)
+# print("10 ning kubi", 10**3)
+
+# print(5+3*5)
+# print(9//2)
+# print(9/2)
+
+# print(5**2)
+# print(5**3)
+# print(100%43)
+
+# print('"Nexia", "Tico", "Damas" ko\'rganlar qilar havas')
+
+# print("5ning 4-darajasi", 5**4 ,"ga teng")
+
+# print("22ni 4 ga bo'lganda", 22%4," qoldiq qoladi")
+
+# print("tomonlari 125ga teng bo'lgan kvadratning yuzi va perimetri""\nprimetri:", 4*125,"\nyuzi:", 125**2,"ga teng" )
+
+
+
+# # Foydalanuvchi tomonni kiritishni so'raymiz va uni butun songa (int)ga o'giramiz
+# tomon = int(input("Kvadratning tomonini kiriting: "))
+
+# Hisoblashlar
+# perimetr = 4 * tomon
+# yuzi = tomon ** 2
+
+#Natijalarni konsolga chiqarish
+
+# print("Kvadratning perimeti:", perimetr)
+# print("Kvadratning yuzi:", yuzi)
+
+# ism = 'suxrob'
+# yosh = 21
+# print(ism, yosh, "yoshda")
+# ism = input("Ismingiz nima? ")
+# yosh = input("Yoshingiz nechida? ")
+# print(ism, yosh, "yoshda")
+
+
+
+# ism = input("Ismingiz nima?\n")
+# print("assalaomu alaykum, " + ism)
+
+# ism = input("Ismingiz nima Farrukh?\n>>>>")
+# print("assalomu alaykum, " + ism.title())
+
+
+
+# kocha = "Bog'bon"
+# mahalla = "Sog'bon"
+# tuman = "Bodomzor"
+# viloyat ="Samarqand"
+# print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati" )
+
+# kocha =input("ko'changizni kiriting:\n<<<")
+# mahalla =input("mahallangizni kiriting:\n<<<")
+# tuman = input("tumaningizni kiriting:\n<<<")
+# viloyat = input("Viloyatingizni kiriting:\n<<<")
+# print(f"{kocha} ko'chasi, {mahalla} mahallasi,\n {tuman} tumani, {viloyat} viloyati.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
